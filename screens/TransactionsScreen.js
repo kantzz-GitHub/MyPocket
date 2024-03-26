a2b39c4755f506e8310ce2faa8fd80a18c72ea0e
Shermukhammad Usmonov <codingblyatt@gmail.com>
2024-03-26 19:49:26 -0400

    Implemented Tabs

---
+++ screens/TransactionsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TransactionsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Transactions Screen</Text>
+    </View>
+  );
+};
+
+export default TransactionsScreen;
