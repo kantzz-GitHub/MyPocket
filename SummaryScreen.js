16f5e4eb82fb9d50f8f452f8f6a0075bfa518039
Shermukhammad Usmonov <codingblyatt@gmail.com>
2024-04-09 16:36:58 -0400

    Tidying up the code

--- SummaryScreen.js
+++ SummaryScreen.js
@@ -1,31 +1,3 @@
-// import React from 'react';
-// import { View, Text } from 'react-native';
-// import { transactions } from './TransactionsScreen';
-
-// const SummaryScreen = () => {
-//   // Calculate total number of items
-//   const totalItems = transactions.length;
-
-//   // Calculate total amount spent
-//   const totalSpent = transactions.reduce((total, transaction) => total + parseFloat(transaction.amount.replace('$', '')), 0).toFixed(2);
-
-//   // Find highest and lowest expenses
-//   const sortedTransactions = [...transactions].sort((a, b) => parseFloat(b.amount.replace('$', '')) - parseFloat(a.amount.replace('$', '')));
-//   const topExpense = sortedTransactions[0];
-//   const lowestExpense = sortedTransactions[sortedTransactions.length - 1];
-
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text style={{fontSize: 20}}>Total Transactions: {totalItems}</Text>
-//       <Text style={{fontSize: 20}}>Total Spent: ${totalSpent}</Text>
-//       <Text style={{fontSize: 20}}>Top Expense: {topExpense.name} - {topExpense.amount}</Text>
-//       <Text style={{fontSize: 20}}>Lowest Expense: {lowestExpense.name} - {lowestExpense.amount}</Text>
-//     </View>
-//   );
-// };
-
-// export default SummaryScreen;
-
 import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
